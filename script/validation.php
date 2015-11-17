096b725a944000a7f8bf1b764d952ee8f05e869a
kansey <kansey@users.noreply.github.com>
2015-11-17 15:22:40 +0700

    Update validation.php

--- script/validation.php
+++ script/validation.php
@@ -91,11 +91,10 @@
 	public function clean($arr)
 	 	{
 	 		foreach ($arr as $str) {
-	 			
-	 		$str = trim($str);
-    			$str = stripslashes($str);
-    			$str = strip_tags($str);
-    			$str = htmlspecialchars($str);
+	 			$str = trim($str);
+    				$str = stripslashes($str);
+    				$str = strip_tags($str);
+    				$str = htmlspecialchars($str);
   			}
 
   			return;
